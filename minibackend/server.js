d426465112097856053eff32bd68363fbfa6156c
vishalmahle11 <vishalmahale974@gmail.com>
2022-07-03 21:59:38 +0530

    mini backend

---
+++ minibackend/server.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+require('dotenv').config()
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8000; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port,
+    
+    console.log(`listning on ${port}`));
